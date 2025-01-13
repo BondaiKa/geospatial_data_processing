287b05c1a130e6a6e63f7e8d776a5ff1583ab4be
Karim Safiullin <st.herbinar@gmail.com>
2025-01-13 21:43:47 +0100

    Create first version of geospatial data processing solution (#2)
    * Restructure a bit code
    
    * Test pre-commit hooks
    
    * Add poetry files and pre-commit
    
    * Pre-commit is working
    
    * Update README
    
    * Add version to just to show image. Requirements are not passed yet. Add logging
    
    * Add version to just to show image. Requirements are not passed yet. Add logging
    
    * Simplify setup config
    
    * Separate analysis notebook and first pipeline notebook, add required libs in poetry
    
    * In the middle of writing first geospatial data processing pipeline
    
    * Rename project I need to get rid off `-`
    
    * Finish super complicated case with 4 tiles
    
    * Finally move to main.py my concatednaed tiles images logic.
    
    * Move places of own typing vars and logging initi

---
+++ main.py
@@ -0,0 +1,161 @@
+import copy
+import logging
+import pathlib
+from pathlib import Path
+from typing import List, Tuple
+
+from PIL import Image
+
+from utils import (
+    concat_two_tile_images,
+    concat_vertically,
+    convert_epsg_4326_to_epsg_25832,
+    convert_epsg_25832_to_epsg_4326,
+    generate_tile_image,
+    get_bounding_box,
+    is_bbox_intersects,
+)
+
+logging.basicConfig(format="[%(asctime)s] [%(levelname)s] %(message)s", level=logging.INFO)
+log = logging.getLogger(__name__)
+
+# Typing
+Latitude = float
+Longitude = float
+
+LatitudeLeft = float
+LongitudeBottom = float
+LatitudeRight = float
+LatitudeTop = float
+
+
+def validate_input(latitude: float, longitude: float, radius: float = 100, epsg_25832=False) -> None:
+    log.info(f"Incoming latitude '{latitude}' and longitude '{longitude}' with set radius '{radius}'.")
+
+    if not epsg_25832 and (not -90 < latitude < 90 and not -180 < longitude < 180):
+        warning_message = (
+            "Expected to have Geographic CRS `EPSG:4326` input format (in degress). Looks "
+            "like provided input data is in `EPSG:25832`. In case of using directly EPSG:25832, "
+            "set 'epsg_25832=True'!"
+        )
+        log.warning(warning_message)
+
+    if radius < 0 or radius > 100:
+        log.warning(f"Radius expected to be from 0 meters till 100 meters. Received {radius} meters.")
+
+
+def get_paths_of_tile_images(
+    image_bounded_box: Tuple[LatitudeLeft, LongitudeBottom, LatitudeRight, LatitudeTop],
+    dataset_directory_full_path: pathlib.Path,
+) -> List[pathlib.Path]:
+
+    relevant_tiles = []
+
+    for image_file in dataset_directory_full_path.glob("*.jp2"):
+        tile_bounded_box = get_bounding_box(image_file)
+        if not tile_bounded_box:
+            error_message = "Something goes wrong with parsing image"
+            log.error(error_message)
+            # TODO @Karim: handle this error better later.
+            raise Exception(error_message)
+
+        if is_bbox_intersects(tile_bounded_box, image_bounded_box):
+            relevant_tiles.append(image_file)
+
+    logging.info(f"Found {len(relevant_tiles)} tile(s) intersecting the required area.")
+    return relevant_tiles
+
+
+def get_image(
+    latitude: float,
+    longitude: float,
+    dataset_directory_path: str,
+    radius: float = 100,
+    epsg_25832=False,
+    final_image_size: Tuple[int, int] = (256, 256),
+) -> Image.Image:
+    """Generate a 256x256 pixels image for the input (latitude, longitude) in
+    EPSG:4326/EPSG:25832 format with a given radius in meters.
+
+    - 'latitude' and 'longitude' must be in 'EPSG:4326' or 'EPSG:25832' format.
+    - 'radius' value expected to be '0' till '100' meters.
+    - 'dataset_directory_path' - orthophotos directory full path
+        (E.g. '/Users/ksafiullin/src/geospatial-data-processing/data/orthophotos/nw').
+
+    Steps:
+    1) Verify incoming data is on EPSG:4326 format.
+    2) Compute bounding box: (left, bottom, right, top).
+    3) Find all relevant tiles that intersect with this bounding box.
+    4) Partially read from each tile.
+    5) Concatenate the partial reads into one final image.
+    6) Crop/resize to 256x256.
+    """
+    log.debug(f"Reading set input data: lat {latitude}, long: {longitude}, and radius {radius}.")
+    validate_input(latitude=latitude, longitude=longitude, radius=radius, epsg_25832=False)
+
+    # If it's EPSG:4326 and not EPSG:25832 format, transforms degrees to meters
+    if not epsg_25832:
+        latitude, longitude = convert_epsg_4326_to_epsg_25832(latitude=latitude, longitude=longitude)
+
+    latitude_left = latitude - radius
+    longitude_bottom = longitude - radius
+    latitude_right = latitude + radius
+    longitude_top = longitude + radius
+
+    final_image_bounding_box = (latitude_left, longitude_bottom, latitude_right, longitude_top)
+
+    tile_image_paths = get_paths_of_tile_images(final_image_bounding_box, Path(dataset_directory_path))
+
+    if not tile_image_paths:
+        # TODO @Karim: I need to consider case when I selected last image in dataset (e.g no image below and right).
+        # So I need to create partial black image and concat with exist tile.
+        log.warning("No tiles intersecting the bounding box. Returning a blank 256x256 image.")
+        return Image.new("RGB", final_image_size, color="black")
+
+    if len(tile_image_paths) == 1:
+        return generate_tile_image(final_image_bounding_box, tile_image_paths[0]).resize(
+            (256, 256), Image.BILINEAR  # type: ignore
+        )
+
+    if len(tile_image_paths) == 2:
+        return (
+            concat_two_tile_images(final_image_bounding_box, tile_image_paths)
+            .resize((256, 256), Image.BILINEAR)  # type: ignore
+            .show()
+        )
+
+    if len(tile_image_paths) == 4:
+        _tile_image_paths = copy.deepcopy(tile_image_paths)
+        _tile_image_paths.sort()
+        return concat_vertically(
+            concat_two_tile_images(final_image_bounding_box, [_tile_image_paths[1], _tile_image_paths[3]]),
+            concat_two_tile_images(final_image_bounding_box, [_tile_image_paths[0], _tile_image_paths[2]]),
+        ).resize(
+            (256, 256), Image.BILINEAR  # type: ignore
+        )
+    else:
+        error_message = (
+            "I didn't implement other cases with missing images in dataset. "
+            "Only when there is no found tile images at all"
+        )
+        log.error(error_message)
+        raise NotImplementedError(error_message)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format="[%(asctime)s] [%(levelname)s] %(message)s", level=logging.INFO)
+
+    # In the middle coordinates of 'dop10rgbi_32_468_5772_1_nw_2022' image file.
+    # Expected to provide 1 tile  (cutted  in the middle 'dop10rgbi_32_468_5772_1_nw_2022' image)
+    # get_image(
+    #     latitude=8.54010563577907,
+    #     longitude=52.10215462837978,
+    #     dataset_directory_path="/Users/ksafiullin/src/geospatial_data_processing/data/orthophotos/nw",
+    #     radius=100,
+    # ).show()
+
+    # Test on the corner of 'dop10rgbi_32_468_5772_1_nw_2022', will it provide 4 tiles or not.
+    get_image(
+        *convert_epsg_25832_to_epsg_4326(468002, 5772002),
+        dataset_directory_path="/Users/ksafiullin/src/geospatial_data_processing/data/orthophotos/nw",
+    ).show()
---
+++ utils.py
@@ -0,0 +1,149 @@
+import logging
+import pathlib
+import re
+from functools import lru_cache
+from typing import List, Tuple
+
+import numpy as np
+import rasterio
+from PIL import Image
+from pyproj import Transformer
+
+logging.basicConfig(format="[%(asctime)s] [%(levelname)s] %(message)s", level=logging.INFO)
+log = logging.getLogger(__name__)
+
+# Typing
+Latitude = float
+Longitude = float
+
+LatitudeLeft = float
+LongitudeBottom = float
+LatitudeRight = float
+LatitudeTop = float
+
+
+TransformerFromCRS = lru_cache(Transformer.from_crs)
+
+
+def get_bounding_box(
+    image_filepath: pathlib.Path,
+) -> Tuple[LatitudeLeft, LongitudeBottom, LatitudeRight, LatitudeTop]:
+    """Return 1000x1000 meters bounding box"""
+    pattern = r"dop10rgbi_32_(\d+)_(\d+)_1_nw_\d{4}\.jp2"
+    match = re.match(pattern, image_filepath.name)
+    if not match:
+        raise ValueError(
+            f"Incorrect filepath. Got {image_filepath}," "expected 'dop10rgbi_32_<easting>_<northing>_1_nw_<year>.jp2'"
+        )
+
+    latitude_left = int(match.group(1)) * 1000
+    latitude_right = latitude_left + 999
+    longitude_bottom = int(match.group(2)) * 1000
+    latitude_top = longitude_bottom + 999
+
+    return (latitude_left, longitude_bottom, latitude_right, latitude_top)
+
+
+def is_bbox_intersects(tile_bbox, final_image_bbox) -> bool:
+    """Return boolean if tile image and final generated image overlap or not."""
+    (tile_bbox_left, tile_bbox_bottom, tile_bbox_right, tile_bbox_top) = tile_bbox
+    (final_image_bbox_left, final_image_bbox_bottom, final_image_bbox_right, final_image_bbox_top) = final_image_bbox
+
+    if tile_bbox_right < final_image_bbox_left or tile_bbox_left > final_image_bbox_right:
+        return False
+    if tile_bbox_top < final_image_bbox_bottom or tile_bbox_bottom > final_image_bbox_top:
+        return False
+
+    return True
+
+
+def convert_epsg_4326_to_epsg_25832(latitude: Latitude, longitude: Longitude) -> Tuple[Latitude, Longitude]:
+    log.debug(
+        f"Converting from 'EPSG:4326' (latitude,longitude) ({latitude}^, {longitude}^) degrees to 'EPSG:25832' meters."
+    )
+    wgs84_to_utm32 = TransformerFromCRS("EPSG:4326", "EPSG:25832", always_xy=True)
+    latitude, longitude = wgs84_to_utm32.transform(xx=latitude, yy=longitude)
+    log.debug(f"Converted to (latitude, longitude) ({latitude},{longitude}) meters")
+    return (latitude, longitude)
+
+
+def convert_epsg_25832_to_epsg_4326(latitude: float, longitude: float) -> Tuple[Latitude, Longitude]:
+    log.debug(f"Converting from 'EPSG:25832' (latitude, longitude) ({latitude}m, {longitude}m) to 'EPSG:4326' degrees.")
+    utm32_to_wgs84 = TransformerFromCRS("EPSG:25832", "EPSG:4326", always_xy=True)
+    latitude, longitude = utm32_to_wgs84.transform(xx=latitude, yy=longitude)
+    log.debug(f"Converted to (latitude, longitude): ({latitude}, {longitude}) degrees")
+    return (latitude, longitude)
+
+
+def concat_hortizontally(left_image, right_image):
+    dst = Image.new("RGB", (left_image.width + right_image.width, left_image.height))
+    dst.paste(left_image, (0, 0))
+    dst.paste(right_image, (left_image.width, 0))
+    return dst
+
+
+def concat_vertically(top_image, bottom_image):
+    dst = Image.new("RGB", (top_image.width, top_image.height + bottom_image.height))
+    dst.paste(top_image, (0, 0))
+    dst.paste(bottom_image, (0, top_image.height))
+    return dst
+
+
+def get_file_latitude_longitude(
+    tile_image_path: pathlib.Path,
+) -> Tuple[Latitude, Longitude]:
+    pattern = r"dop10rgbi_32_(\d+)_(\d+)_1_nw_\d{4}\.jp2"
+    match = re.match(pattern, tile_image_path.name)
+    if match is None:
+        # TODO @Karim: Need to handle the case when it's different file name convention
+        raise ValueError(f"Invalid file name format: {tile_image_path.name}")
+    return (float(match.group(1)), float(match.group(2)))
+
+
+def generate_tile_image(
+    tile_bounding_box: Tuple[LatitudeLeft, LongitudeBottom, LatitudeRight, LatitudeTop],
+    tile_image_path: pathlib.Path,
+):
+    log.info(f"Generating {tile_image_path.stem} tile image")
+    with rasterio.open(tile_image_path) as src:
+        window = rasterio.windows.from_bounds(
+            *tile_bounding_box,
+            transform=src.transform,
+        )
+        log.info(window)
+        partial_rgb = src.read([1, 2, 3], window=window)
+        log.info(partial_rgb.shape)
+
+        partial_rgb = np.transpose(partial_rgb, (1, 2, 0))
+        return Image.fromarray(partial_rgb, mode="RGB")
+
+
+def concat_two_tile_images(
+    final_image_bounding_box: Tuple[LatitudeLeft, LongitudeBottom, LatitudeRight, LatitudeTop],
+    tile_image_paths: List[pathlib.Path],
+):
+    first_tile_langitude, first_tile_longitude = get_file_latitude_longitude(tile_image_paths[0])
+    second_tile_langitude, second_tile_longitude = get_file_latitude_longitude(tile_image_paths[1])
+
+    if first_tile_langitude == second_tile_langitude:
+        if first_tile_longitude < second_tile_longitude:
+            return concat_vertically(
+                generate_tile_image(final_image_bounding_box, tile_image_paths[0]),
+                generate_tile_image(final_image_bounding_box, tile_image_paths[1]),
+            )
+        else:
+            return concat_vertically(
+                generate_tile_image(final_image_bounding_box, tile_image_paths[1]),
+                generate_tile_image(final_image_bounding_box, tile_image_paths[0]),
+            )
+    elif first_tile_longitude == second_tile_longitude:
+        if second_tile_langitude > first_tile_langitude:
+            return concat_hortizontally(
+                generate_tile_image(final_image_bounding_box, tile_image_paths[0]),
+                generate_tile_image(final_image_bounding_box, tile_image_paths[1]),
+            )
+        else:
+            return concat_hortizontally(
+                generate_tile_image(final_image_bounding_box, tile_image_paths[1]),
+                generate_tile_image(final_image_bounding_box, tile_image_paths[0]),
+            )
